337b627bc839770502a710c189ad2bb9b3b3fe91
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2023-11-08 17:32:50 +0100

    ok

--- tp2/version1/js/main.js
+++ tp2/version1/js/main.js
@@ -18,8 +18,35 @@ validerBtn.addEventListener('click',()=>{
     let email=emailInput.value
     let telephone=telephoneInput.value
     let age=ageInput.value
+    let isValid=true;
+    let inputs = document.querySelectorAll(".inputSection input");
 
     // champ obligatoire
-    
-    show(nom,prenom,email,telephone,age)
+    inputs.forEach(element=>{
+        if(element.value==""){
+            element.classList.add('error')
+            element.classList.remove('valid')
+            element.nextElementSibling.innerText="obligatoire"
+            isValid=false
+        }
+    })
+    if(isValid)
+        {
+        show(nom,prenom,email,telephone,age)
+        inputs.forEach(element=>{
+            element.classList.remove('error')
+            element.classList.remove('valid')
+            element.nextElementSibling.innerText=""
+            })  
+        }
+    else{
+        // affectation des classes valid
+        inputs.forEach(element=>{
+            if(element.value!=""){
+                element.classList.add('valid')
+                element.classList.remove('error')
+                element.nextElementSibling.innerText=""
+            }
+        })
+    }
 })
