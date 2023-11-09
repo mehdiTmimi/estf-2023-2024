351aca05ed9aeb6445e6348ec65d91b9ea2273a6
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2023-11-09 18:04:43 +0100

    ok

--- tp1/groupe2/js/main.js
+++ tp1/groupe2/js/main.js
@@ -7,7 +7,6 @@ let validerBtn = document.getElementById("validerBtn")
 let resultatListe = document.getElementById("resultatListe")
 
 let ajouterResultat = (balise,contenu,color,href)=>{
-    
     // creation des elements
     let li = document.createElement('li')
     let ele = document.createElement(balise)
@@ -21,3 +20,25 @@ let ajouterResultat = (balise,contenu,color,href)=>{
             ele.setAttribute('target','_blank')
         }
 }
+
+validerBtn.addEventListener('click',()=>{
+    // recuperation des valeurs .value
+    let balise = baliseInput.value
+    let contenu = contenuInput.value
+    let color = colorInput.value
+    let href = hrefInput.value
+    //verification (TODO)
+    // appel a la fonction ajouterResultat
+    ajouterResultat(balise,contenu,color,href)
+    // vider le formulaire
+    baliseInput.selectedIndex=0
+    contenuInput.value=""
+    colorInput.value="#000000"
+    hrefInput.value=""
+})
+let resetFct = ()=>{
+    resultatListe.innerHTML=""
+}
+resetBtn.addEventListener('click',()=>{
+    resetFct()
+})
