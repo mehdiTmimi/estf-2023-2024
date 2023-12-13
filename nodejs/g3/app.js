1b21f52c0026b0c819be7c5df7544b39433c1f07
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2023-12-13 18:22:28 +0100

    ok

--- nodejs/g3/app.js
+++ nodejs/g3/app.js
@@ -1,21 +1,53 @@
-const express = require("express")
-const PORT = 3000
-const app = express()
-app.use("/ok",(req,res,next)=>{
-    console.log("inside middleware 1");
-    if(req.method=="GET")
-        res.send("ok")
-    else next()
-})
+const express = require("express");
+const fs = require("fs");
+const { readHtml } = require("./utils");
+const PORT = 3000;
+const app = express();
 
-app.get("/users",(req,res,next)=>{
-    console.log("inside middleware users");
-    res.send("list of users")
+app.use(express.static("./site"))
+app.get("/index.html",(req,res)=>{
+    res.send("oooook")
 })
-app.use((req,res,next)=>{
-    console.log("inside middleware 2");
-    res.send("ok2")
-})
-app.listen(PORT,()=>{
-    console.log("server started at ",PORT);
-})
+app.use((req, res, next) => {
+  res.setHeader("author", "mehdi tmimi");
+  next();
+});
+app.use((req, res, next) => {
+  let ip = req.ip;
+  let method = req.method;
+  let path = req.path;
+  let time = new Date().toString();
+  fs.appendFile(
+    "logs.txt",
+    JSON.stringify({ ip, method, path, time }) + "\n",
+    () => {}
+  );
+  next();
+});
+app.use((req, res, next) => {
+    next()
+  let login = req.header("login");
+  let pwd = req.header("pwd");
+  if (login == "admin" && pwd == "mehdi") return next();
+  res.status(403).send("vous devez se connecter");
+});
+app.get("/users", (req, res) => {
+  let users = [
+    { nom: "tmimi", prenom: "mehdi" },
+    { nom: "alaoui", prenom: "kenza" },
+  ];
+  res.send("users")
+});
+/*app.get("/mehdi.php", (req, res) => {
+  res.send(readHtml("site/index.html"));
+});
+app.get("/css/main.css", (req, res) => {
+    res.setHeader("Content-Type","text/css")
+  res.send(readHtml("site/css/main.css"));
+});*/
+app.use((req, res, next) => {
+  res.status(404).send("sorry");
+});
+app.listen(PORT, () => {
+  console.log("server started at ", PORT);
+});
